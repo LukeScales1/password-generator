d8ffbf5daff76338841a7bef04a3d8f8e02f086d
LukeScales <luke.scales@pointy.com>
2019-05-27 23:10:14 +0100

    Included argparse to vary the password length via command-line argument

--- main/generate_password.py
+++ main/generate_password.py
@@ -1,6 +1,13 @@
 import string
 import random
+import argparse
+
+parser = argparse.ArgumentParser(description='generate a password with x amount of randomised ascii characters')
+parser.add_argument('x', metavar='N', type=int, help='length of desired password string')
+args = parser.parse_args()
+
+x = args.x
 
 password_characters = string.ascii_letters + string.digits + string.punctuation
-secret_key = ''.join(random.choice(password_characters) for i in range(16))
+secret_key = ''.join(random.choice(password_characters) for i in range(x))
 print secret_key
